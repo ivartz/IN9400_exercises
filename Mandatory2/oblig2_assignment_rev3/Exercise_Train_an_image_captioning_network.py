b1cc59a751c343f83b7094c0296058f4d1352a2b
ivartz <djloek@gmail.com>
2019-04-26 10:16:22 +0200

    one more run. Make delivery .zip for Mandatory2 hand in on devilry

--- Mandatory2/oblig2_assignment_rev3/Exercise_Train_an_image_captioning_network.py
+++ Mandatory2/oblig2_assignment_rev3/Exercise_Train_an_image_captioning_network.py
@@ -34,15 +34,15 @@ if __name__ == '__main__':
 
     #train
     modelParam = {
-        'batch_size': 200,  # Training batch size
+        'batch_size': 128,  # Training batch size
         'cuda': {'use_cuda': True,  # Use_cuda=True: use GPU
                  'device_idx': 0},  # Select gpu index: 0,1,2,3
         'numbOfCPUThreadsUsed': 3,  # Number of cpu threads use in the dataloader
-        'numbOfEpochs': 40,  # Number of epochs
+        'numbOfEpochs': 20,  # Number of epochs
         'data_dir': data_dir,  # data directory
         'img_dir': 'loss_images/',
         'modelsDir': 'storedModels/',
-        'modelName': 'model_7/',  # name of your trained model
+        'modelName': 'model_8/',  # name of your trained model
         'restoreModelLast': 0,
         'restoreModelBest': 0,
         'modeSetups': [['train', True], ['val', True]],
@@ -53,14 +53,14 @@ if __name__ == '__main__':
     config = {
         'optimizer': 'adam',  # 'SGD' | 'adam' | 'RMSprop'
         'learningRate': {'lr': 0.0005},  # learning rate to the optimizer
-        'weight_decay': 0.0001,  # weight_decay value
+        'weight_decay': 0,  # weight_decay value
         'VggFc7Size': 4096,  # Fixed, do not change
         'embedding_size': 128,  # word embedding size
-        'vocabulary_size': 3000,  # number of different words
-        'truncated_backprop_length': 20,
+        'vocabulary_size': 4000,  # number of different words
+        'truncated_backprop_length': 7,
         'hidden_state_sizes': 256,  #
-        'num_rnn_layers': 4,  # number of stacked rnn's
-        'cellType': 'GRU'  # RNN or GRU
+        'num_rnn_layers': 2,  # number of stacked rnn's
+        'cellType': 'RNN'  # RNN or GRU
     }
 
     if modelParam['inference'] == True:
